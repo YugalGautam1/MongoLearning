bf2c7a69af98495bbce816258ddda8b4af22c5e4
YugalGautam1 <reachyugal@gmail.com>
2024-02-04 22:36:13 -0500

    Haha

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require("express");
+const mongoose = require('mongoose')
 const articleRouter = require('./routes/articles')
+const Article = require('.model/Article')
 const app = express();
 app.set('view engine','ejs')
 
+mongoose.connect('mongodb://localhost/blog')
+app.use(express.urlencoded({extended:false}))
+
+app.get('/', async (req, res) => {
+  const articles = await Article.find()
+  res.render('articles/index',{articles:articles})
+})
 app.use('/articles',articleRouter)
 
-app.get('/', (req, res) => {
-    const articles = [{
-        title: 'Test Article',
-        createdAt: Date.now(),
-        description: 'Test description'
-    }]
-  res.render('index',{articles:'Hello'})
-})
 app.listen(5000)
