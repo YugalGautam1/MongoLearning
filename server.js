aa00a9a3635c51d3d8c400e4e5b210191ff26755
YugalGautam1 <reachyugal@gmail.com>
2024-03-04 23:26:34 -0500

    hehehe

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-const express = require("express");
+const express = require('express')
 const mongoose = require('mongoose')
-const articleRouter = require('./routes/articles')
-const Article = require('.model/Article')
-const app = express();
-app.set('view engine','ejs')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles.js')
+const methodOverride = require('method-override')
+const app = express()
 
 mongoose.connect('mongodb://localhost/blog')
+app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended:false}))
-
-app.get('/', async (req, res) => {
-  const articles = await Article.find()
-  res.render('articles/index',{articles:articles})
+app.get('/', async (req,res)=>{
+    const articles = await Article.find().sort({createdAt:'desc'})
+    res.render("articles/index", {articles:articles})
 })
-app.use('/articles',articleRouter)
+app.use(methodOverride('_method'))
 
+app.use('/articles',articleRouter)
 app.listen(5000)
